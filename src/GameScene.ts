02bc37d378aee7e3fbd7116aeadf80c193c2332a
Adam Due Hansen <adamduehan@gmail.com>
2021-07-27 08:00:28 +0200

    Pipes are now made of physics sprites.

--- src/GameScene.ts
+++ src/GameScene.ts
@@ -18,7 +18,7 @@ class GameScene extends Scene {
   #animateGround = true;
 
   #groundGroup!: Physics.Arcade.Group;
-  #pipesGroup!: Physics.Arcade.StaticGroup;
+  #pipesGroup!: Physics.Arcade.Group;
   #player!: Types.Physics.Arcade.SpriteWithDynamicBody;
   #playerGroundCollider!: Physics.Arcade.Collider;
   #playerPipeCollider!: Physics.Arcade.Collider;
@@ -49,7 +49,7 @@ class GameScene extends Scene {
     this.createGround();
     this.createGameOver();
 
-    this.#pipesGroup = this.physics.add.staticGroup();
+    this.#pipesGroup = this.physics.add.group();
 
     this.#playerGroundCollider = this.physics.add.collider(
       this.#player,
@@ -103,22 +103,8 @@ class GameScene extends Scene {
   public update(): void {
     this.#groundGroup.children.each((ground) => {
       const image = ground as Types.Physics.Arcade.SpriteWithDynamicBody;
-      if (this.#animateGround) {
-        image.setVelocityX(-200);
-
-        if (image.x + this.cameras.main.width / 2 < 0) {
-          image.x = this.cameras.main.width + 4;
-        }
-      } else {
-        image.setVelocityX(0);
-      }
-    });
-
-    this.#pipesGroup.children.each((pipe) => {
-      if (this.#animateGround) {
-        const image = pipe as GameObjects.Image;
-        image.body.position.x -= 2;
-        image.x -= 2;
+      if (image.x + this.cameras.main.width / 2 < 0) {
+        image.x = this.cameras.main.width + 4;
       }
     });
   }
@@ -153,7 +139,8 @@ class GameScene extends Scene {
     this.#player = this.physics.add
       .sprite(150, this.cameras.main.height / 2, 'yellowbird')
       .setScale(2)
-      .setDepth(2);
+      .setDepth(2)
+      .setImmovable(true);
   }
 
   private createGround(): void {
@@ -209,24 +196,34 @@ class GameScene extends Scene {
       Math.floor(Math.random() * PIPE_TOP_MAX_HEIGHT) + PIPE_TOP_PUSH_UPWARDS;
     const pipeSpawnX = this.cameras.main.width + 50;
 
-    // Top pipe
-    this.#pipesGroup.add(
-      this.add
-        .image(pipeSpawnX, topPipeHeight, 'pipe')
-        .setScale(2)
-        .setOrigin(0.5, 1)
-        .setFlipY(true)
-        .setDepth(0)
-    );
+    const topPipe = this.#pipesGroup.create(
+      pipeSpawnX,
+      topPipeHeight,
+      'pipe'
+    ) as Types.Physics.Arcade.SpriteWithDynamicBody;
+    topPipe
+      .setScale(2)
+      .setOrigin(0.5, 1)
+      .setFlipY(true)
+      .setDepth(0)
+      .setVelocityX(-200)
+      .setImmovable(true);
+    topPipe.body.setAllowGravity(false);
 
-    // Bottom pipe
-    this.#pipesGroup.add(
-      this.add
-        .image(pipeSpawnX, topPipeHeight + 200, 'pipe')
-        .setScale(2)
-        .setOrigin(0.5, 0)
-        .setDepth(0)
-    );
+    // TODO: Gap
+
+    const bottomPipe = this.#pipesGroup.create(
+      pipeSpawnX,
+      topPipeHeight + 200,
+      'pipe'
+    ) as Types.Physics.Arcade.SpriteWithDynamicBody;
+    bottomPipe
+      .setScale(2)
+      .setOrigin(0.5, 0)
+      .setDepth(0)
+      .setVelocityX(-200)
+      .setImmovable(true);
+    bottomPipe.body.setAllowGravity(false);
   }
 
   private newGame(): void {
@@ -235,6 +232,7 @@ class GameScene extends Scene {
     this.pushPlayer();
     this.physics.resume();
     this.#stageRunning = true;
+    this.#player.body.setAllowGravity(true);
   }
 
   private gameOver(): void {
@@ -247,17 +245,21 @@ class GameScene extends Scene {
     this.#animateGround = false;
     this.#gameOverFadeInTween.play();
     this.#spawnPipesEvent.paused = true;
+    this.#groundGroup.setVelocityX(0);
+    this.#pipesGroup.setVelocityX(0);
   }
 
   public resetStage(): void {
     this.#player.setAlpha(0);
-    this.#message.setAlpha(0);
     this.#player.setY(this.cameras.main.height / 2);
-    this.physics.pause();
+    this.#player.body.setAllowGravity(false);
+    this.#player.setVelocityY(0);
     this.#player.play({
       key: 'flap',
       repeat: -1,
     });
+    this.#message.setAlpha(0);
+    this.#groundGroup.setVelocityX(-200);
     this.#gameFadeInTween.play();
     this.#enablePlayerControl = true;
     this.#playerGroundCollider.active = true;
